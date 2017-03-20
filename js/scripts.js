33e9aecdc559227e24865cb947920c7eb6490374
Dominic Pace <monstadiablo@gmail.com>
2017-03-20 08:23:18 -0400

    commented our fixed navbar section

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,9 @@ $(document).ready(function(){
   $('#fadein').fadeIn(400);
 });
 
-$(window).scroll(function(){
-  var scroll = $(window).scrollTopx();
-  if (scroll <= 500) {
-    $(".navbar-transparent").removeClass(".navbar-transparent").addClass("dark-navbar").addClass("navbar-fixed-top");
-  }
-});
+// $(window).scroll(function(){
+//   var scroll = $(window).scrollTopx();
+//   if (scroll <= 500) {
+//     $(".navbar-transparent").removeClass(".navbar-transparent").addClass("dark-navbar").addClass("navbar-fixed-top");
+//   }
+// });
